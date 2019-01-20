5941dff1baa983d992358649d761f768fb762843
Mr-Manna <mr.manna87@gmail.com>
2019-01-20 16:47:01 +0530

    Go templates created

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func main(){
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.HandleFunc("/", func( w http.ResponseWriter, r *http.Request ){
+		if err := templates.ExecuteTemplate( w, "index.html", nil); err!= nil{
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		// fmt.Fprintf( w, " Hello World! ")
+	})
+	
+	fmt.Println(http.ListenAndServe( ":8000",nil))
+	}
