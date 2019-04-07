d69a9a9efb36cf0137ce6a844defaacb2d3413ac
Mr-Manna <mr.manna87@gmail.com>
2019-04-07 16:07:48 +0530

    go server and template

--- server.go
+++ server.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
+	"database/sql"
+	_"github.com/mattn/go-sqlite3"
 )
 
 type Page struct{
 	Name string
+	DBStatus bool
 }
 
 func main(){
 	templates := template.Must(template.ParseFiles("templates/index.html"))
+	fmt.Println("Server Is On")
+	//  connecting Database
+	db, _ := sql.Open("sqlite3","dev.db")
 
 	http.HandleFunc("/", func( w http.ResponseWriter, r *http.Request ){
 		p := Page{ Name: "Gopher"}
@@ -19,6 +25,8 @@ func main(){
 		if name := r.FormValue("name"); name != " " {
 			p.Name = name
 			}
+		// http://localhost:8000/?name=name to pass the variable
+			p.DBStatus = db.Ping() == nil
 
 		if err := templates.ExecuteTemplate( w, "index.html", p); err!= nil{
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -26,5 +34,5 @@ func main(){
 		// fmt.Fprintf( w, " Hello World! ")
 	})
 	
-	fmt.Println(http.ListenAndServe( ":8000",nil))
+	fmt.Println(http.ListenAndServe( ":8888",nil))
 	}
